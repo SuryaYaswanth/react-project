17b34261a5c4e04037f65b99cab2b160d4efe335
Surya <104778234+SuryaYaswanth@users.noreply.github.com>
2024-10-27 16:39:53 +0530

    Add files via upload

---
+++ calculatorApp/Calculator.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "/src/style.css";
+const Calculator = () => {
+    const[inputValue,setInputValue] = useState('');
+    
+    const display = (value) => {setInputValue(inputValue+value)}
+    const clear = () =>{
+        setInputValue('');
+    }
+    const calculate = () =>{
+      setInputValue(eval(inputValue));
+    }
+  return (
+    <form className="calculator" >
+        <input type="text" className="value" value={inputValue}/>
+
+        <span className="clear" onClick={()=> clear()}>
+            C
+        </span>
+        <span onClick={() => display("/")}>/</span>
+        <span onClick={() => display("*")}>*</span>
+        <span onClick={() => display("7")}>7</span>
+        <span onClick={() => display("8")}>8</span>
+        <span onClick={() => display("9")}>9</span>
+        <span onClick={() => display("-")}>-</span>
+        <span onClick={() => display("4")}>4</span>
+        <span onClick={() => display("5")}>5</span>
+        <span onClick={() => display("6")}>6</span>
+        <span className="plus" onClick={() => display("+")}>+</span>
+
+        <span onClick={() => display("1")}>1</span>
+        <span onClick={() => display("2")}>2</span>
+        <span onClick={() => display("3")}>3</span>
+        <span onClick={() => display("0")}>0</span>
+        <span onClick={() => display("00")}>00</span>
+        <span onClick={() => display(".")}>.</span>
+        <span className="equal" onClick={() => calculate()}>
+            =
+        </span>
+       
+    </form>
+  )
+}
+
+export default Calculator;
